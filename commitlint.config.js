b9a3e506cdaad6040f2054981f76b67824ddcdd0
juanalbaladejo-aircury <juan.albaladejo@aircury.com>
2024-04-10 12:11:08 +0200

    feat: add rules to Commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,6 @@
+/*
+ * @commitlint/config-conventional sets the conventional rules for commit messages, such as prefixing the commit type (feat, fix, chore, etc.) followed by a descriptive message.
+ */
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+};
